e3d8aa15cdfcbf14041b5f9e47dab65e81c2589e
sfbarts <sfbedu@gmail.com>
2023-07-31 15:08:39 -0400

    Exercise 8.5, implement allBooks by genre filter

--- library-backend/index.js
+++ library-backend/index.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks(author: String): [Book]
+    allBooks(author: String, genre: String): [Book]
     allAuthors: [Author]
   }
 `;
@@ -131,11 +131,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books;
       }
 
-      return books.filter((b) => b.author === args.author);
+      if (args.author) {
+        return books.filter((b) => b.author === args.author);
+      }
+
+      return books.filter((b) => b.genres.includes(args.genre));
     },
     allAuthors: () => authors,
   },
