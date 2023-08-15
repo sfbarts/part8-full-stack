7085219dbf1e936d9859ecf3cc4528f106e6a79e
sfbarts <sfbedu@gmail.com>
2023-08-15 11:56:34 -0400

    Use react router to organize navigation

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
@@ -7,19 +8,38 @@ const App = () => {
   const [page, setPage] = useState("authors");
 
   return (
-    <div>
+    <Router>
       <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <div>
+          <Link to="/authors">
+            <button onClick={() => setPage("authors")}>authors</button>
+          </Link>
+          <Link to="/books">
+            <button onClick={() => setPage("books")}>books</button>
+          </Link>
+          <Link to="/new-book">
+            <button onClick={() => setPage("add")}>add book</button>
+          </Link>
+        </div>
+
+        <Routes>
+          <Route
+            path="/authors"
+            element={<Authors show={page === "authors"} />}
+          />
+          <Route path="/books" element={<Books show={page === "books"} />} />
+          <Route path="/new-book" element={<NewBook show={page === "add"} />} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <h2>Books App - Select An Option Above</h2>
+              </div>
+            }
+          />
+        </Routes>
       </div>
-
-      <Authors show={page === "authors"} />
-
-      <Books show={page === "books"} />
-
-      <NewBook show={page === "add"} />
-    </div>
+    </Router>
   );
 };
 
